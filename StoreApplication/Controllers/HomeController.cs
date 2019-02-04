2a5a1ab4bf8d9a6efb45899e37f2e724a982c0db
Saima Chaity <saima.s.chaity@gmail.com>
2019-02-04 10:23:07 -0800

    Added cookies, session, pagination on product list view model

--- StoreApplication/Controllers/HomeController.cs
+++ StoreApplication/Controllers/HomeController.cs
@@ -3,35 +3,111 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Pages.Account.Internal;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StoreApplication.Data;
 using StoreApplication.Models;
+using StoreApplication.Repositories;
+using StoreApplication.Services;
 
 namespace StoreApplication.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public StoreDBContext db;
+        public ApplicationDbContext userdb;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public HomeController(StoreDBContext db,
+            IHttpContextAccessor httpContextAccessor,
+            SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager)
         {
-            return View();
+            this.db = db;
+            this._httpContextAccessor = httpContextAccessor;
+            this._userManager = userManager;
+            this._signInManager = signInManager;
         }
 
-        public IActionResult About()
+        [HttpPost]
+        public ActionResult Index(UserSearch userSearch)
         {
-            ViewData["Message"] = "Your application description page.";
+            CookieHelper cookieHelper = new CookieHelper(_httpContextAccessor, Request,
+                                                         Response);
 
-            return View();
+            cookieHelper.Set("searchString", userSearch.searchString, 1);
+
+            return RedirectToAction("Index");
         }
 
-        public IActionResult Contact()
+        [HttpGet]
+        public IActionResult Index(string sortOrder, string searchString, int? page, UserSearch userSearch)
         {
-            ViewData["Message"] = "Your contact page.";
+            CookieHelper cookieHelper = new CookieHelper(_httpContextAccessor, Request,
+                                                         Response);
+            if (_signInManager.IsSignedIn(User))
+            {
+                string userID = _userManager.GetUserId(User);
+                cookieHelper.Set("UserID", userID, 1);
+                HttpContext.Session.SetString("UserID", userID);
+                ViewBag.userId = HttpContext.Session.GetString("UserID");
+            }
+            if (!_signInManager.IsSignedIn(User))
+            {
+                ViewBag.userId = "";
+            }
+            if (User.Identity.Name != null)
+            {
+                var userName = User.Identity.Name;
+                if (HttpContext.Session.GetString(userName) == null)
+                {
+                    string name = userName;
+                    ViewBag.name = name;
+                    HttpContext.Session.SetString("username", name);
 
-            return View();
+                }
+                else
+                {
+                    ViewBag.name = HttpContext.Session.GetString(userName);
+                }
+            }
+
+            string userSearchString = cookieHelper.Get("searchString");
+
+            string sort = String.IsNullOrEmpty(sortOrder) ? "title_asc" : sortOrder;
+            string search = String.IsNullOrEmpty(searchString) ? "" : searchString;
+
+            ViewData["CurrentSort"] = sort;
+            ViewData["CurrentFilter"] = search;
+            int pageSize = 2;
+
+            if (userSearchString != null)
+            {
+                ViewBag.searchString = userSearchString;
+
+                var queryWithUserSearchString = new ProductRepo(db).GetAll(sort, userSearchString);
+
+                return View(PaginatedList<Product>.Create(queryWithUserSearchString, page ?? 1, pageSize));
+            }
+
+            else
+            {
+                var query = new ProductRepo(db).GetAll(sort, search);
+                return View(PaginatedList<Product>.Create(query, page ?? 1, pageSize));
+            }
         }
 
-        public IActionResult Privacy()
+        public IActionResult ClearCookie(string key)
         {
-            return View();
+            CookieHelper cookieHelper = new CookieHelper(_httpContextAccessor, Request,
+                                                         Response);
+            cookieHelper.Remove(key);
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ StoreApplication/Repositories/ProductRepo.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using StoreApplication.Data;
+using StoreApplication.Models;
+using StoreApplication.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreApplication.Repositories
+{
+    public class ProductRepo
+    {
+        public StoreDBContext db;
+
+        public ProductRepo(StoreDBContext db)
+        {
+            this.db = db;
+        }
+
+        public IQueryable<Product> GetAll(string sortOrder, string searchString)
+        {
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+
+                var query = from p in db.Product
+                            where (p.ProductName.StartsWith(searchString))
+                            select p;
+
+                return query;
+
+            }
+            else
+            {
+                var query = from p in db.Product
+                            select p;
+
+                switch (sortOrder)
+                {
+                    case "title_asc":
+                        query =
+                            query.OrderBy(p => p.ProductName);
+                        break;
+                    case "title_desc":
+                        query =
+                            query.OrderByDescending(p => p.ProductName);
+                        break;
+                    case "price_desc":
+                        query =
+                      query.OrderByDescending(p => p.Price);
+                        break;
+                    default:
+                        query =
+                            query.OrderBy(p => p.Price);
+                        break;
+                };
+                return query;
+            }
+        }
+
+
+    }
+}
