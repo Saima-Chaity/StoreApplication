5ba387c802d63eb4ae272a0f2fcc411a039dbdd4
Saima Chaity <saima.s.chaity@gmail.com>
2019-02-16 20:21:17 -0800

    Added create role action-view

--- StoreApplication/Controllers/RoleController.cs
+++ StoreApplication/Controllers/RoleController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StoreApplication.Data;
 using StoreApplication.Repositories;
+using StoreApplication.ViewModels;
 
 namespace StoreApplication.Controllers
 {
@@ -23,6 +24,29 @@ namespace StoreApplication.Controllers
             RoleRepo roleRepo = new RoleRepo(_context);
             return View(roleRepo.GetAllRoles());
         }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(RoleVM roleVM)
+        {
+            if (ModelState.IsValid)
+            {
+                RoleRepo roleRepo = new RoleRepo(_context);
+                var success = roleRepo.CreateRole(roleVM.RoleName);
+                if (success)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            ViewBag.Error = "An error occurred while creating this role. Please try again.";
+            return View();
+        }
+
     }
 
 }
