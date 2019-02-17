2d01cd5f5d028b6e074be2b1e463babaf5004b8a
Saima Chaity <saima.s.chaity@gmail.com>
2019-02-16 20:31:15 -0800

    Created action to get all users from database

---
+++ StoreApplication/Controllers/UserRoleController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StoreApplication.Data;
+using StoreApplication.Repositories;
+using StoreApplication.ViewModels;
+
+namespace StoreApplication.Controllers
+{
+    public class UserRoleController : Controller
+    {
+        private ApplicationDbContext _context;
+        private IServiceProvider _serviceProvider;
+
+        public UserRoleController(ApplicationDbContext context,
+                                    IServiceProvider serviceProvider)
+        {
+            _context = context;
+            _serviceProvider = serviceProvider;
+        }
+
+        public ActionResult Index()
+        {
+            UserRepo userRepo = new UserRepo(_context);
+            var users = userRepo.All();
+            return View(users);
+        }
+    }
+
+}
